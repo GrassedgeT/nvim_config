66d262ed1592bd5aaedfad5ed2d9097a27b80098
GrassedgeT <grassedge@outlook.com>
2024-03-29 12:24:59 +0800

    vscode-nvim-whichkey

--- vscode-nvim/init.lua
+++ vscode-nvim/init.lua
@@ -1,1 +1,10 @@
+require("options")
+require("keymaps")
+require("autocmds")
+
+local opt = vim.opt
+
+-- Set the default shiftwidth to 4
+opt.shiftwidth = 4
+
  
---
+++ vscode-nvim/lua/options.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+
+local opt = vim.opt
+
+opt.tabstop = 4
+opt.shiftwidth = 2
