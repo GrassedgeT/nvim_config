f6a017e1bbbfe1781d36beee62915ab1929c2f07
GrassedgeT <grassedge@outlook.com>
2024-04-16 17:17:22 +0800

    消除警告

--- vscode-nvim/init.lua
+++ vscode-nvim/init.lua
@@ -1,10 +1,9 @@
-require("options")
-require("keymaps")
-require("autocmds")
+--require("options")
+--require("keymaps")
+--require("autocmds")
 
 local opt = vim.opt
 
 -- Set the default shiftwidth to 4
 opt.shiftwidth = 4
 
- 
